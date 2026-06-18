1ee0de0655ea13b812481902cc8d813376130f53
den3099 <dennisarce99@gmail.com>
2026-06-18 10:43:41 -0600

    Add function getComputerChoice
    The function generates a random number, rounded to floor, between 0 and 2 to choose rock, paper or scissors and shows the value in the console

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+function getComputerChoice () {
+    let computerChoice = Math.floor(Math.random() * 3);
+    switch (computerChoice) {
+        case 0:
+            computerChoice = "rock";
+            break;
+        case 1:
+            computerChoice = "paper";
+            break;
+        case 2:
+            computerChoice = "scissors";
+            break;
+    }
+    return computerChoice;
+}
+
+console.log(getComputerChoice());

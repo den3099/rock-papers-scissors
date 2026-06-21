ecffd16937d7dbae689d8907173410b4db084eae
den3099 <dennisarce99@gmail.com>
2026-06-21 17:16:14 -0600

    Add buttons for the player and change scoring logic
    The prompt was changed to buttons, now the user could select an option without input error's from the prompt
    Now the score isn't counted inside a while loop, when either the player or the computer reaches 5 points, they reset to 0

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 let humanScore = 0;
 let computerScore = 0;
+let humanChoice = "";
+
+let playerButtons = document.querySelector("#player-choices");
+
+
+playerButtons.addEventListener("click", (event) => {
+    let target = event.target;
+    
+    switch (target.id) {
+        case "rock":
+            humanChoice = "rock";
+            scoreCounter(humanScore, computerScore);
+            break;
+        case "paper":
+            humanChoice = "paper";
+            scoreCounter(humanScore, computerScore);
+            break;
+        case "scissors":
+            humanChoice = "scissors";
+            scoreCounter(humanScore, computerScore);
+            break;
+    }
+    console.log(`\n`);
+});
+
 
 function getComputerChoice () {
     let computerChoice = Math.floor(Math.random() * 3);
@@ -17,10 +42,6 @@ function getComputerChoice () {
     return computerChoice;
 }
 
-function getHumanChoice () {
-    let humanChoice = prompt("Select rock, paper, or scissors: ");
-    return humanChoice;
-}
 
 function playRound (humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
@@ -28,46 +49,55 @@ function playRound (humanChoice, computerChoice) {
     switch (humanChoice) {
         case "rock":
             if (computerChoice === "rock") {
-                return "It's a tie!";
+                return console.log("It's a tie!");
             } else if (computerChoice === "paper") {
                 computerScore++;
-                return "You lose! Paper beats rock.";
+                return console.log("You lose! Paper beats rock.");
             } else {
                 humanScore++;
-                return "You win! Rock beats scissors.";
+                return console.log("You win! Rock beats scissors.");
             }
         case "paper":
             if (computerChoice === "rock") {
                 humanScore++;
-                return "You win! Paper beats rock.";
+                return console.log("You win! Paper beats rock.");
             } else if (computerChoice === "paper") {
-                return "It's a tie!";
+                return console.log("It's a tie!");
             } else {
                 computerScore++;
-                return "You lose! Scissors beats paper.";
+                return console.log("You lose! Scissors beats paper.");
             }
         case "scissors":
             if (computerChoice === "rock") {
                 computerScore++;
-                return "You lose! Rock beats scissors.";
+                return console.log("You lose! Rock beats scissors.");
             } else if (computerChoice === "paper") {
                 humanScore++;
-                return "You win! Scissors beats paper.";
+                return console.log("You win! Scissors beats paper.");
             } else {
-                return "It's a tie!";
+                return console.log("It's a tie!");
             }
          default:
-            return "Invalid input. Please select rock, paper, or scissors.";
+            return console.log("Invalid input. Please select rock, paper, or scissors.");
     }
 }
 
-while (humanScore < 5 && computerScore < 5) {
-    console.log(playRound(getHumanChoice(), getComputerChoice()));
-    console.log(`Current score - Player: ${humanScore} Computer: ${computerScore}`);
-}
 
-if (humanScore === 5) {
-    console.log("Congratulations! You won the game!");
-} else {
-    console.log("Sorry, you lost the game. Better luck next time!");
-}
+function scoreCounter (humanScore, computerScore) {
+    playRound(humanChoice, getComputerChoice());
+    if (humanScore === 5) {
+        console.log("Congratulations! You won the game!");
+        scoreReset();
+    } else if (computerScore === 5) {
+        console.log("Sorry, you lost the game. Better luck next time!");
+        scoreReset();
+    } else {
+        console.log(`Current Score - You: ${humanScore} Computer: ${computerScore}`);
+    };
+};
+
+
+function scoreReset () {
+    humanScore = 0;
+    computerScore = 0;
+};

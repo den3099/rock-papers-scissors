7effa8e0fb5779e5a805be6402a543e152721d69
den3099 <dennisarce99@gmail.com>
2026-06-22 10:47:18 -0600

    Change scoreCounter function and add 2 buttons
    Now the scoreCounter function uses global variables instead of parameters, a button for 'Start Game' and 'Reset Game' were added to make the score returns to 0

--- app.js
+++ app.js
@@ -9,17 +9,28 @@ playerButtons.addEventListener("click", (event) => {
     let target = event.target;
     
     switch (target.id) {
+        case "start-game":
+        case "reset-game":
+            humanScore = 0;
+            computerScore = 0;
+            console.log("Game started! First to 5 wins. Please select rock, paper, or scissors.");
+            console.log(`Current Score - You: ${humanScore} Computer: ${computerScore}`);
+            break;
         case "rock":
             humanChoice = "rock";
-            scoreCounter(humanScore, computerScore);
+            scoreCounter();
             break;
         case "paper":
             humanChoice = "paper";
-            scoreCounter(humanScore, computerScore);
+            scoreCounter();
             break;
         case "scissors":
             humanChoice = "scissors";
-            scoreCounter(humanScore, computerScore);
+            scoreCounter();
+            break;
+        default:
+            humanScore = 0;
+            computerScore = 0;
             break;
     }
     console.log(`\n`);
@@ -44,8 +55,6 @@ function getComputerChoice () {
 
 
 function playRound (humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
     switch (humanChoice) {
         case "rock":
             if (computerChoice === "rock") {
@@ -54,7 +63,7 @@ function playRound (humanChoice, computerChoice) {
                 computerScore++;
                 return console.log("You lose! Paper beats rock.");
             } else {
-                humanScore++;
+                humanScore++;   
                 return console.log("You win! Rock beats scissors.");
             }
         case "paper":
@@ -83,7 +92,7 @@ function playRound (humanChoice, computerChoice) {
 }
 
 
-function scoreCounter (humanScore, computerScore) {
+function scoreCounter () {
     playRound(humanChoice, getComputerChoice());
     if (humanScore === 5) {
         console.log("Congratulations! You won the game!");
